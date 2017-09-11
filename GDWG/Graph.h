813c12b9c3664d055826ddae192551b7a9aa8fe9
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-09-12 00:11:49 +1000

    progress towards edges & test2

--- GDWG/Graph.h
+++ GDWG/Graph.h
@@ -54,20 +54,26 @@ namespace gdwg {
 			Node(N val) : data{val} { }
 			N getData() const { return data; }
 			size_t outDegree() const { return edges.size(); }
+			bool addEdge(const Edge& e);
+			void showEdges() const;
 		private:
 			N data; //a Node consists of its data of type N
-			std::vector<std::weak_ptr<Edge>> edges; //and a vector of its outgoing Edges
+			std::vector<std::shared_ptr<Edge>> edges; //and a vector of its outgoing Edges
 		};
 
 		class Edge {
 		public:
-
+			Edge(std::shared_ptr<Node> src, std::shared_ptr<Node> dst, E w) :
+				src{src}, dst{dst}, weight{w} { }
+			std::shared_ptr<Node> getDest() const { return dst; }
+			E getWeight() const { return weight; }
 		private:
-			Node src; //an Edge consists of a source Node,
-			Node dest; //a destination Node
+			std::shared_ptr<Node> src; //an Edge consists of a source Node,
+			std::shared_ptr<Node> dst; //a destination Node
 			E weight; //and a weight of type E
 		};
 
+		auto getNode(const N& val) const;
 	};
 
 	#include "Graph.tem"
